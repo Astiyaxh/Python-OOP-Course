2eceb30cf4ebefda86da67a5c49e6c9f3f3d3043
Astiyaxh <ariyan.mehri00@gmail.com>
2022-12-21 07:41:26 +0330

    I'm Back. Section 4 Is Complete.

--- Python-OOP_04.py
+++ Python-OOP_04.py
@@ -10,7 +10,7 @@ class Employee:
         self.first = first
         self.last = last 
         self.pay = pay
-        self.email = first + '.' + last + '@company.com'
+        self.email = first + '.' + last + '@email.com'
 
         Employee.num_of_emps += 1
     
@@ -22,15 +22,64 @@ class Employee:
 
 
 class Developer(Employee):
-    pass
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        # Employee.__init__(self, first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    
+    def print_emps(self):
+        for emp in self.employees:
+            print('--->', emp.fullname())
+
 
 emp_1 = Employee('Reza', 'Mehri', 500000)
 emp_2 = Employee('Test', 'User', 600000)
 
-dev_1 = Developer('Corey', 'Schafer', 500000)
-dev_2 = Developer('Hello', 'World', 600000)
+dev_1 = Developer('Corey', 'Schafer', 500000, 'Python')
+dev_2 = Developer('Hello', 'World', 600000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
 # print(help(Developer))
 
 # print(dev_1.email)
 # print(dev_2.email)
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+
+# print(dev_1.prog_lang)
+# print(dev_2.prog_lang)
+
+# print(mgr_1.email)
+
+# mgr_1.remove_emp(dev_1)
+# mgr_1.add_emp(dev_2)
+
+# mgr_1.print_emps()
+
+
+print(isinstance(mgr_1, Employee))
+print(isinstance(mgr_1, Developer))
+print(issubclass(Manager, Employee))
+print(issubclass(Developer, Employee))
